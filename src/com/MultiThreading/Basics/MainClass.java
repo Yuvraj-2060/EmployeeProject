7fb5d6ebc196ccdf09e7c2fbf0df8866bba36a70
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-04-24 15:21:13 +0530

    Multhreading & Different Ways Of Creating It.

---
+++ src/com/MultiThreading/Basics/MainClass.java
@@ -0,0 +1,11 @@
+package com.MultiThreading.Basics;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+    }
+
+}
