37fdf64f04c5100837ae678065a22073af51a63f
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-05-04 23:13:08 +0530

    BST main method  Implementation

---
+++ src/DataStructure/BST/Node.java
@@ -0,0 +1,21 @@
+package DataStructure.BST;
+
+public class Node {
+
+    int key;
+    String value;
+
+    Node leftChild, rightChild;
+
+    public Node(int key, String value) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [key=" + key + ", value=" + value + "]";
+    }
+
+}
